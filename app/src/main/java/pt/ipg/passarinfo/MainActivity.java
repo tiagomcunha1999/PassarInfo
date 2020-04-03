e507e9471bd2cc0b791597fa14f443f4c12f1f39
Tiago Cunha <1700597@sal.ipg.pt>
2020-04-03 18:44:43 +0100

    Chegada à parte do codigo dos Intents

--- app/src/main/java/pt/ipg/passarinfo/MainActivity.java
+++ app/src/main/java/pt/ipg/passarinfo/MainActivity.java
@@ -2,13 +2,46 @@ package pt.ipg.passarinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText Nome = (EditText) findViewById(R.id.editText);
+        String message = Nome.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        EditText Idade = (EditText) findViewById(R.id.editText2);
+        String message = Idade.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        EditText editText = (EditText) findViewById(R.id.editText4);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        EditText editText = (EditText) findViewById(R.id.editText5;
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        EditText editText = (EditText) findViewById(R.id.editText6);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
